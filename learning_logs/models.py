867efb315cad33113c7591309b84067b0d1190f1
Oluwatobiloba Ezekiel <mroluwatobilobaezekiel@gmail.com>
2024-10-27 12:57:39 +0100

    created a superuser and redefined the models that allows user to add topics

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -7,3 +7,19 @@ class Topic(models.Model):
 
     def __str__(self) -> str:
         return self.text
+    
+class Entry(models.Model):
+
+    """CASCADE tells it to delete everything under it 
+    once the topic is deleted"""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class meta:
+        """Change the name to entries once there are more than one entry"""
+        verbose_name_plural = 'entries'
+
+    def __str__(self) -> str:
+        """return the first 50 character of the topic and ..."""
+        return f"{self.text[:50]}..."
