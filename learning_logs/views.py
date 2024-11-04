c11132423c1f94f794ee06f43ffe6ca34e522a1d
Oluwatobiloba Ezekiel <mroluwatobilobaezekiel@gmail.com>
2024-11-04 15:52:36 +0100

    working on topics

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import Topic
 
 # Create your views here.
 def index(request):
     """Learning Logs Homepage route"""
     return render(request,'learning_logs/index.html')
+
+def topics(request):
+    """Topics page route"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
